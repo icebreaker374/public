8fd6d5c1a838a11e9d278bb17649d46a3d4ae867
icebreaker374 <38338154+icebreaker374@users.noreply.github.com>
2023-09-06 12:41:23 -0400

    Create HideSearchAndTaskView.ps1

---
+++ scripting/Intune/HideSearchAndTaskView.ps1
@@ -0,0 +1,3 @@
+﻿New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -Name 'SearchBoxTaskbarMode' -PropertyType 'DWord' -Value 0
+
+New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\Currentversion\Explorer\Advanced' -Name 'ShowTaskViewButton' -PropertyType 'DWord' -Value 0
