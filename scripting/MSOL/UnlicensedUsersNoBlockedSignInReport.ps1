8180bc8ed815f38774a841ab1dbadccf0ca34fad
icebreaker374 <38338154+icebreaker374@users.noreply.github.com>
2023-09-06 12:13:34 -0400

    Update UnlicensedUsersNoBlockedSignInReport.ps1

--- scripting/MSOL/UnlicensedUsersNoBlockedSignInReport.ps1
+++ scripting/MSOL/UnlicensedUsersNoBlockedSignInReport.ps1
@@ -11,10 +11,42 @@ md Temp > $null
 Write-Host "A directory called 'Temp' was created at 'C:\'"
 }
 
+if(Get-InstalledModule MSOnline){ # This curly bracket opens the if/else statement that checks if the MSOnline module is installed.
+
+    Write-Host "
+    Confirmed the MSOnline PowerShell module is installed."
+
+    if(Get-Module MSOnline){ # This curly bracket opens the if/else statement that checks if the MSOnline module is loaded.
+
+        Write-Host "
+        Confirmed the MSOnline PowerShell Module is loaded."
+    }
+
+    else{
+    
+        Import-Module MSOnline
+
+        Write-Host "
+        The MSOnline PowerShell Module was successfully loaded."
+    } # This curly bracket closes the if/else statement that checks if the MSOnline module is loaded.
+
+} # This curly bracket closes the if/else statement that checks if the MSOnline module is installed.
+
+else{
+
+    Install-Module MSOnline -Scope CurrentUser
+    Import-Module MSOnline
+
+    Write-Host "
+    The MSOnline PowerShell module was successfully installed and loaded."
+} # This is where the checks for C:\Temp, installed modules, and imported modules end.
+
 Write-Host "You will be prompted to login with User/Global Admin credentials to connet to MSOnline in 5 seconds..."
 
 Connect-MsolService
 
-Write-Host "A list of unlicensed users that DO NOT have sign-in blocked will be generated:"
+$ReportExportPath = $PWD.ToString() + "\OpenUnlicensedUsersReport.csv" # This command defines the export path for the report with unlicensed users that DO NOT have sign-in blocked.
 
-Get-MsolUser -All -EnabledFilter EnabledOnly| Where-Object {($_.isLicensed -eq $false) -and ($_.UserPrincipalName -NotMatch "onmicrosoft.com") -and ($_.UserPrincipalName -NotMatch "EXT")} | Select-Object UserPrincipalName | Export-CSV C:\Temp\ActiveUnlicensedUsers.csv
+Write-Host "A list of unlicensed users that DO NOT have sign-in blocked will be generated at" $ReportExportPath.ToString()
+
+Get-MsolUser -All -EnabledFilter EnabledOnly| Where-Object {($_.isLicensed -eq $false) -and ($_.UserPrincipalName -NotMatch "onmicrosoft.com") -and ($_.UserPrincipalName -NotMatch "EXT")} | Select-Object UserPrincipalName | Export-CSV $ReportExportPath
