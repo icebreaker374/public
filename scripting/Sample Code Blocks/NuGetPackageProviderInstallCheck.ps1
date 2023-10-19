e7a272015f21186aec3392c0239d69a3185545a9
icebreaker374 <38338154+icebreaker374@users.noreply.github.com>
2023-10-18 21:11:43 -0400

    Created sample code blocks

---
+++ scripting/Sample Code Blocks/NuGetPackageProviderInstallCheck.ps1
@@ -0,0 +1,40 @@
+Write-Host "
+Checking if the NuGet package provider is installed..."
+
+$NuGetInstallationCheck = Get-PackageProvider
+
+if($NuGetInstallationCheck.Name -NotContains "NuGet"){
+
+    Write-Host "
+Attempting to install the NuGet package provider..."
+    
+    Install-PackageProvider -Name NuGet -Scope CurrentUser -ErrorAction SilentlyContinue
+
+    Write-Host "
+Verifying if the NuGet package provider is installed"
+    
+    $NuGetInstallationVerification = Get-PackageProvider
+
+    if($NuGetInstallationVerification.Name -Contains "NuGet"){
+
+        Write-Host "
+Confirmed the NuGet package provider was installed"
+    }
+
+    else{
+
+        Write-Host "
+Failed to install the NuGet package provider.  Please attempt to manually run: 'Install-PackageProvider -Name NuGet -Scope CurrentUser'."
+
+        Write-Host "
+The script will exit in 5 seconds..."
+
+        exit
+        }
+}
+
+else{
+
+    Write-Host "
+Confirmed the NuGet package provider is installed."
+}
