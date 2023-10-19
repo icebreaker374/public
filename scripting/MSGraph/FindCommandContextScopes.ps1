be6544b091d40b6f4ebe4d736f4f5a7e0ab8e047
icebreaker374 <38338154+icebreaker374@users.noreply.github.com>
2023-10-18 21:59:53 -0400

    Wrote script to get context scopes for graph commands

---
+++ scripting/MSGraph/FindCommandContextScopes.ps1
@@ -0,0 +1,9 @@
+﻿$Cmdlet = Read-Host "Please enter the cmdlet you want to view the available context scopes for"
+
+((Find-MgGraphCommand -Command $Cmdlet).Permissions.Name) | Select @{N='AvailableContextScopes'; E={$_}} | Out-String
+
+Read-Host "Press Enter to exit"
+
+# Referenced content:
+
+#Referenced URL: https://stackoverflow.com/questions/42708128/show-output-before-read-host Referenced Content: $GetSQL = Get-SqlVersions | Out-String
