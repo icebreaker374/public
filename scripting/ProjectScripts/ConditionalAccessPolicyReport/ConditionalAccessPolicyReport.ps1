b2ff1d7991446b88cec741bb3eb5af21ead00a4a
icebreaker374 <apaiva@rmonnetworks.com>
2023-10-21 00:47:29 -0400

    Added excluded users check

--- scripting/ProjectScripts/ConditionalAccessPolicyReport/ConditionalAccessPolicyReport.ps1
+++ scripting/ProjectScripts/ConditionalAccessPolicyReport/ConditionalAccessPolicyReport.ps1
@@ -2,54 +2,111 @@
 
 foreach($policy in $Policies){
 
-    Write-Host $policy.DisplayName
+    $PolicyName = $policy.DisplayName
+
+    Write-Host "----------------------------------------"
+    
+    Write-Host "Conditional Access Policy Name: '$PolicyName'"
 
     # Begin if/else statement for policy status.
     
     if($policy.State -EQ "enabled"){
 
-        $PolicyStatus = "Status: ENABLED"
+        $PolicyStatus = "Policy Status: ENABLED"
     }
 
     elseif($policy.State -EQ "disabled"){
 
-        $PolicyStatus = "Status: DISABLED"
+        $PolicyStatus = "Policy Status: DISABLED"
     }
 
     else{
 
-        $PolicyStatus = "Status: UNKOWN"
+        $PolicyStatus = "Policy Status: UNKOWN"
     }
 
     # End if/else statement for policy status.
 
     Write-Host $PolicyStatus
-    Write-Host "
-"
+    Write-Host ""
+
     # Begin if/else statement for included users check.
     
     if($policy.Conditions.Users.IncludeUsers -EQ "All"){
 
         $IncludedUsers = "Included users: All Users"
+
+        Write-Host $IncludedUsers
     }
 
     elseif(($policy.Conditions.Users.IncludeUsers -NE "All") -and ($policy.Conditions.Users.IncludeUsers -NE $null)){
 
-        $Users = foreach($user in $policy.Conditions.Users.IncludeUsers){
+        $Users = @()
+        
+        foreach($user in $policy.Conditions.Users.IncludeUsers){
 
-            Get-AzureADUser -ObjectId $user | Select DisplayName
+            $UserDisplayName = Get-AzureADUser -ObjectId $user | Select DisplayName
+
+            $Users += $UserDisplayName.DisplayName
         }
 
-        $IncludedUsers = "Included users: $Users"
+        $IncludedUsers = $Users
+
+        Write-Host "Included Users:"
+
+        foreach($useraccount in $Users){
+            
+            Write-Host $useraccount
+        }
     }
 
     else{
 
-        $IncludedUsers = "Included users: UNKNOWN"
+        $IncludedUsers = "Included users: NONE"
+
+        Write-Host $IncludedUsers
     }
 
-    Write-Host "Included Users:" $Users.DisplayName
-    Write-Host "
-"
+    # End if/else statement for included users check.  Begin if/else statement for excluded users check.
 
+    if($policy.Conditions.Users.ExcludeUsers -EQ "All"){
+
+        $ExcludedUsers = "Excluded users: All Users"
+
+        Write-Host ""
+        Write-Host $ExcludedUsers
+    }
+
+    elseif(($policy.Conditions.Users.ExcludeUsers -NE "All") -and ($policy.Conditions.Users.ExcludeUsers -NE $null)){
+
+        $Users = @()
+        
+        foreach($user in $policy.Conditions.Users.ExcludeUsers){
+
+            $UserDisplayName = Get-AzureADUser -ObjectId $user | Select DisplayName
+
+            $Users += $UserDisplayName.DisplayName
+        }
+
+        $ExcludedUsers = $Users
+
+        Write-Host ""
+        Write-Host "Excluded Users:"
+
+        foreach($useraccount in $Users){
+            
+            Write-Host $useraccount
+        }
+    }
+
+    else{
+
+        $ExcludedUsers = "Excluded users: NONE"
+
+        Write-Host ""
+        Write-Host $ExcludedUsers
+    }
+
+    Write-Host "----------------------------------------"
+    Write-Host ""
 }
