68e50977fc373a6b81e687f73018083ef9e867aa
apaiva-rmon <apaiva@rmonnetworks.com>
2023-11-26 01:06:39 -0500

    Update NuGetPackageProviderInstallCheck.ps1

--- scripting/Sample Code Blocks/NuGetPackageProviderInstallCheck.ps1
+++ scripting/Sample Code Blocks/NuGetPackageProviderInstallCheck.ps1
@@ -1,33 +1,27 @@
-Write-Host "
-Checking if the NuGet package provider is installed..."
+Write-Host "Checking if the NuGet package provider is installed..."
 
 $NuGetInstallationCheck = Get-PackageProvider
 
 if($NuGetInstallationCheck.Name -NotContains "NuGet"){
 
-    Write-Host "
-Attempting to install the NuGet package provider..."
+    Write-Host "Attempting to install the NuGet package provider..."
     
     Install-PackageProvider -Name NuGet -Scope CurrentUser -ErrorAction SilentlyContinue
 
-    Write-Host "
-Verifying if the NuGet package provider is installed"
+    Write-Host "Verifying if the NuGet package provider is installed."
     
     $NuGetInstallationVerification = Get-PackageProvider
 
     if($NuGetInstallationVerification.Name -Contains "NuGet"){
 
-        Write-Host "
-Confirmed the NuGet package provider was installed"
+        Write-Host "Confirmed the NuGet package provider was installed."
     }
 
     else{
 
-        Write-Host "
-Failed to install the NuGet package provider.  Please attempt to manually run: 'Install-PackageProvider -Name NuGet -Scope CurrentUser'."
+        Write-Host "Failed to install the NuGet package provider.  Please attempt to manually run: 'Install-PackageProvider -Name NuGet -Scope CurrentUser'."
 
-        Write-Host "
-The script will exit in 5 seconds..."
+        Write-Host "The script will exit in 5 seconds..."
 
         exit
         }
@@ -35,6 +29,5 @@ The script will exit in 5 seconds..."
 
 else{
 
-    Write-Host "
-Confirmed the NuGet package provider is installed."
+    Write-Host "Confirmed the NuGet package provider is installed."
 }
