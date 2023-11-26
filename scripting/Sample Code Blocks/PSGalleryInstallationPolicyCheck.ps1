2c3c9b20e0c21fd69e48ff8428f177043b00f393
apaiva-rmon <apaiva@rmonnetworks.com>
2023-11-25 22:59:56 -0500

    Update PSGalleryInstallationPolicyCheck.ps1

--- scripting/Sample Code Blocks/PSGalleryInstallationPolicyCheck.ps1
+++ scripting/Sample Code Blocks/PSGalleryInstallationPolicyCheck.ps1
@@ -1,31 +1,27 @@
-Write-Host "
-Checking if PSGallery is a trusted repository..."
+Write-Host "Checking if PSGallery is a trusted repository..."
 
 $PSRepository = Get-PSRepository -Name PSGallery
 
 if($PSRepository.InstallationPolicy -NE "Trusted"){
 
-    Write-Host "
-Attempting to set PSGallery as a trusted repository..."
+    Write-Host "Attempting to set PSGallery as a trusted repository..."
 
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -ErrorAction SilentlyContinue
 
-    Write-Host "
-Verifying if PSGallery is now a trusted repository..."
+    Write-Host "Verifying if PSGallery is now a trusted repository..."
 
     if($PSRepository.InstallationPolicy -Match "Trusted"){
 
-        Write-Host "
-Confirmed PSGallery is now a trusted repository."
+        Write-Host "Confirmed PSGallery is now a trusted repository."
     }
 
     else{
 
-        Write-Host "
-Failed to set PSGallery as a trusted repository.  Please attempt to manually run: 'Set-PSRepository -Name PSGallery -InstallationPolicy Trusted'."
+        Write-Host "Failed to set PSGallery as a trusted repository.  Please attempt to manually run: 'Set-PSRepository -Name PSGallery -InstallationPolicy Trusted'."
 
-        Write-Host "
-The script will exit in 5 seconds..."
+        Write-Host "The script will exit in 5 seconds..."
+
+        Start-Sleep -Seconds 5
 
         exit
     }
@@ -33,6 +29,5 @@ The script will exit in 5 seconds..."
 
 else{
 
-    Write-Host "
-Confirmed PSGallery is already a trusted repository."
+    Write-Host "Confirmed PSGallery is already a trusted repository."
 }
